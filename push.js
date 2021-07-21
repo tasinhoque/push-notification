eb12dd4dc8f5e83d2a01ae50900f9e732fe26cb2
Tasin Hoque <tasinhoque@gmail.com>
2021-07-21 13:07:54 +0600

    Complete the event listener

--- push.js
+++ push.js
@@ -12,6 +12,15 @@ push.setVapidDetails(
   vapidKeys.privateKey
 )
 
-let sub = {}
+let sub = {
+  endpoint:
+    'https://wns2-sg2p.notify.windows.com/w/?token=BQYAAADzwOYDczHfncdt22BTLLubdVo%2fd7D0AKTqWnUm%2byhpQl1f4kNxUkfl%2bLNM6AGWN9A3M6FUVgi8D%2bse0rLl9M6soXGBi%2fXjKqG5H9flUmsHZNMy%2fXcvT51aNjXir8fSZv3yCMwELTTDnlpz3OhmDcNHhb3hUhqGk2kgd4TPPt43Z4RSTDcaR1tGrSXwPwQAwsP8WkaWVhXFBjtA7aWF6MQczcR2MwHNoZsRWtuYpci4%2fB13XVtxlbMIsuhXcQBqOmMec%2fENo0LqNdZg0QUjEdkwyQZoZjS3DIRAYp6mXASaeHnJM9QIqPJyMtIrW%2f%2f2gaBTkrrH3U0iWuHxoFr2pIeDiMuVwClZuPm179uWELl47g%3d%3d',
+  expirationTime: null,
+  keys: {
+    p256dh:
+      'BEZAjwOKxgmdsMJluPm7riTiXzlnvH_EMh65wsRDsarzImnf6s4DBo_QmoGp7g2Wm9BB3ISKttZ7FNXPQgZAmf4',
+    auth: 'HJ6aOS7ONUJ_BIN8VvxHrw',
+  },
+}
 
 push.sendNotification(sub, 'test message')
